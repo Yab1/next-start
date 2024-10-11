7ea7fbfb8d0f633509ccf3e459556a5fa16288d3
yab1 <yabilisanu@gmail.com>
2024-10-11 20:02:43 +0300

    feat(ui): add theme switcher to home page
    - integrated `ThemeSwitcher` component into the home page
    - updated layout to include a title and description within a card
    - styled card for better presentation and alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { Card, CardBody } from "@nextui-org/react";
+
+import { ThemeSwitcher } from "@/components/theme-switcher";
+
+export default async function Home() {
+  return (
+    <Card className="mx-auto mt-4 max-w-md">
+      <CardBody className="flex flex-col items-center">
+        <h1 className="text-5xl">Next.js Starter</h1>
+        <p className="mb-10 text-xl">A simple starter for Next.js</p>
+        <ThemeSwitcher />
+      </CardBody>
+    </Card>
+  );
 }
