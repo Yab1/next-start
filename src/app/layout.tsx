2112ddd9e0580697071b444e2947927a8ea76b7b
yab1 <yabilisanu@gmail.com>
2024-10-31 17:14:31 +0300

    feat(layout): wrap layout with providers and update body styling
    added Providers wrapper to layout for global state and UI context
    updated body structure with main content styling and background image support
    introduced conditional background images for light and dark themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 
+import Providers from "@/components/providers";
+
+import "./globals.css";
+
 export const metadata: Metadata = {
   title: "Next.js Starter App",
   description: "A basic starter for next.js",
@@ -16,7 +20,13 @@ export default function RootLayout({
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
       ></link>
-      <body>{children}</body>
+      <body className="flex h-screen w-screen flex-col">
+        <Providers>
+          <main className="flex-grow overflow-auto bg-[url(/light-bg.svg)] bg-cover dark:bg-[url(/dark-bg.svg)]">
+            {children}
+          </main>
+        </Providers>
+      </body>
     </html>
   );
 }
