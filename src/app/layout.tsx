17b7938329537935a64c92843171743af0edba2b
yab1 <yabilisanu@gmail.com>
2024-12-15 12:34:29 +0300

    build(setup): set up environment with Atlassian Design System dependencies and configurations
    Installed core dependencies: @atlaskit/css-reset and @atlaskit/tokens for consistent styling and dark mode support.
    Configured Babel to support design tokens with @atlaskit/tokens/babel-plugin in babel.config.js.
    Set up Parcel bundler with @compiled/parcel-config for efficient CSS handling.
    Updated Webpack configuration with @compiled/webpack-loader and mini-css-extract-plugin for optimized CSS extraction.
    Installed and configured ESLint plugin @atlaskit/eslint-plugin-design-system for in-context help and styling standards.
    Added example button component from @atlaskit/button to validate setup.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 
+import "@atlaskit/css-reset";
+
 export const metadata: Metadata = {
   title: "Next.js Starter App",
   description: "A basic starter for next.js",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import Button from "@atlaskit/button/new";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <section>
+      <h1>Hello World</h1>
+      <Button>Hello world</Button>
+    </section>
+  );
 }
