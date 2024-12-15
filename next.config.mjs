d1e6122ac3aaf91f6c67f9f7b13c53fd0ba7efbf
yab1 <yabilisanu@gmail.com>
2024-12-15 12:08:09 +0300

    refactor(config): update next.config.mjs to improve imports and add image settings
    This commit refactors the `next.config.mjs` file:
    - Updated `createJiti` import to use named import syntax.
    - Replaced `jiti` execution with `jiti.import` for improved readability and functionality.
    - Added support for `images.domains` with "localhost" to allow local image hosting.
    - Set `output` to "standalone" for optimized production builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
-import createJiti from "jiti";
+import { createJiti } from "jiti";
 import { fileURLToPath } from "node:url";
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti("./src/env/server.ts");
+jiti.import("./src/env/server.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["localhost"],
+  },
+  output: "standalone",
   experimental: {
     typedRoutes: true,
   },
